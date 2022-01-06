793f7bd7fdcb1ba8d8b7548c6b3e74a390beb9fb
irfansha <irfansha536@gmail.com>
2022-01-06 11:04:51 +0100

    Subtractor circuit complete, testing holds.

--- utils/adder_cir.py
+++ utils/adder_cir.py
@@ -1,5 +1,6 @@
 # Irfansha Shaik, 29.12.2021, Aarhus
 
+# Requirement is that, the number of variables accommodates the sum of two numbers without oveflow
 def add_circuit(gg, vars, num):
     num_variables = len(vars)
     # Representation in binary requires number of variables:
@@ -44,3 +45,32 @@ def add_circuit(gg, vars, num):
 
 
     return sum_output_gates
+
+# We use 2's complement for the subtractor,
+# We have requirement that the result is always positive and we never subtract with 0:
+def sub_circuit(gg, vars, num):
+  num_variables = len(vars)
+  # Representation in binary requires number of variables:
+  rep_string = '0' + str(num_variables) + 'b'
+  bin_string = format(num, rep_string)
+
+  # Computing 1's complement by flipping bits:
+  ones_complement_string = ''
+
+  for char in bin_string:
+    if char == '0':
+      ones_complement_string += '1'
+    else:
+      ones_complement_string += '0'
+
+  # Computing 2's complement by adding one:
+  ones_complement_int = int(ones_complement_string,2)
+
+  twos_complement_int = ones_complement_int + 1
+
+  # now calling the add circuit,
+  # remember that in add circuit we ignore the overflow bit:
+
+  sum_output_gates = add_circuit(gg, vars, twos_complement_int)
+
+  return sum_output_gates
