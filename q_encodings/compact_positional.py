03043a7b5a0876768946e5d945cd0b701b6477a0
irfansha <irfansha536@gmail.com>
2022-06-11 16:35:32 +0200

    Only allowing stuttering from starting if tight neighbours enabled.

--- q_encodings/compact_positional.py
+++ q_encodings/compact_positional.py
@@ -185,9 +185,14 @@ class CompactPositonal:
         self.step_output_gates.append(self.gates_generator.output_gate)
 
     # Allowing stuttering, if P_i = P_{i+1} then P_{i+1} = P_{i+2}:
-    # only stuttering above the lower bound:
-    #print(self.parsed.lower_bound_path_length-1, self.safe_max_path_length-2)
-    for i in range(self.safe_max_path_length-2):
+    # if using tight pruning the stuttering starts from the beginning:
+    if (self.parsed.args.tight_neighbour_pruning == 1):
+      start_index = 0
+    else:
+      # only stuttering above the lower bound:
+      start_index = self.parsed.lower_bound_path_length-1
+    for i in range(start_index, self.safe_max_path_length-2):
+      #for i in range(self.safe_max_path_length-2):
       # First equality:
       self.gates_generator.complete_equality_gate(self.witness_variables[i], self.witness_variables[i+1])
       first_equality_output_gate = self.gates_generator.output_gate
