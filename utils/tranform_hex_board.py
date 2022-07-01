c1e2807fd3fc5099fe86bd057d0f26c73e3bd035
irfansha <irfansha536@gmail.com>
2022-07-01 15:07:45 +0200

    Additional preprocessing based on hypergraph. - for human played games, it can have an impact

--- utils/tranform_hex_board.py
+++ utils/tranform_hex_board.py
@@ -315,6 +315,119 @@ if __name__ == '__main__':
         temp_simplified_positions.append(pos)
     simplified_positions = list(temp_simplified_positions)
   #=====================================================================================================================================
+
+
+
+  #=====================================================================================================================================
+  # Computing hyper graph:
+  #-------------------------------------------------------------------------------
+  minimal_unreachable_path_nodes = []
+  if (args.prune_minimal_path_unreachable_nodes == 1):
+    min_G = nx.Graph()
+
+    for key,value_list in simplified_neighbour_dict.items():
+      for value in value_list:
+        min_G.add_edge(key, value)
+
+    max_path_length = int((len(parsed_dict["#times"][0]) + 1)/2)
+
+    all_final_paths = []
+
+    for start in new_int_start_boarder:
+      for end in new_int_end_boarder:
+        paths = nx.all_simple_paths(min_G, source=start, target=end, cutoff=max_path_length-1)
+        plist = list(paths)
+
+        set_list = []
+        for path in plist:
+          set_list.append(set(path))
+
+        # sorting the set_list based on path lengths:
+        set_list.sort(key=len)
+        # For now, quadratic complexity loops:
+
+        final_small_paths = []
+
+        list_for_avail_paths = list(set_list)
+        # checking each path one at a time:
+        for path in set_list:
+          remove_path_list = []
+          # if the current path is not in the avail list then it has been supersumed and remove already, we do not need to do it again:
+          if path not in list_for_avail_paths:
+              continue
+          for temp_path in list_for_avail_paths:
+            intersection = set.intersection(path, temp_path)
+            if path == intersection:
+              remove_path_list.append(temp_path)
+            # if the temp_path in the avialable paths is same as intersection, that means the temp_path is same as the path we are checking for:
+            if (temp_path == intersection):
+              assert(path == temp_path)
+          # first removing the large paths:
+          for remove_path in remove_path_list:
+            list_for_avail_paths.remove(remove_path)
+          # adding the current small path:
+          final_small_paths.append(path)
+        if (len(final_small_paths) != 0):
+          # adding all the final small paths:
+          for small_path in final_small_paths:
+            small_path = list(small_path)
+            small_path.sort()
+            all_final_paths.append(list(small_path))
+          #print(start, end)
+          #print(final_small_paths)
+    # iterate through the paths and compute the unreachable nodes:
+    touched_positions = []
+    for win in all_final_paths:
+      for pos in win:
+        if (pos not in touched_positions):
+          touched_positions.append(pos)
+    for sim_pos in simplified_positions:
+      if sim_pos not in touched_positions:
+        minimal_unreachable_path_nodes.append(sim_pos)
+    #print(minimal_unreachable_path_nodes)
+  #-------------------------------------------------------------------------------
+  # dropping the minimal unreachable positions:
+  if (args.prune_minimal_path_unreachable_nodes == 1):
+    # first trim the neighbour dict based on minimal unreachability:
+    pruned_minimal_simplified_neighbour_dict = dict()
+    # Simplifying nieghbours based on minimal unreachability:
+    for key,neighbour_list in simplified_neighbour_dict.items():
+      if key in minimal_unreachable_path_nodes:
+        continue
+      temp_list = []
+      for one_neighbour in neighbour_list:
+        # we do not add the black initial positions, should not be present here:
+        assert(one_neighbour not in black_initial_positions)
+        if (one_neighbour in minimal_unreachable_path_nodes):
+          continue
+        temp_list.append(one_neighbour)
+      if len(temp_list) == 0:
+        continue
+      pruned_minimal_simplified_neighbour_dict[key] = temp_list
+
+    simplified_neighbour_dict = pruned_minimal_simplified_neighbour_dict.copy()
+
+    # pruning the  empty nodes in the start and end boarder nodes, due to unreachability:
+    temp_start_boarder = []
+    for pos in new_int_start_boarder:
+      if pos in simplified_neighbour_dict:
+        temp_start_boarder.append(pos)
+    new_int_start_boarder = list(temp_start_boarder)
+
+    temp_end_boarder = []
+    for pos in new_int_end_boarder:
+      if pos in simplified_neighbour_dict:
+        temp_end_boarder.append(pos)
+    new_int_end_boarder = list(temp_end_boarder)
+
+    # pruning the empty nodes in positions due to unreachability:
+    temp_simplified_positions = []
+    for pos in simplified_positions:
+      if pos in simplified_neighbour_dict:
+        temp_simplified_positions.append(pos)
+    simplified_positions = list(temp_simplified_positions)
+
+  #=====================================================================================================================================
   # printing input files:
   print("#blackinitials")
   print("#whiteinitials")
