70396333b4ff0b3528b31eb088e464a1ed4ad141
irfansha <irfansha536@gmail.com>
2023-01-23 10:16:33 +0100

    expanding data frame generator

--- other_scripts/create_dataframe_general_games.py
+++ other_scripts/create_dataframe_general_games.py
@@ -5,7 +5,6 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import math
 import argparse
 
 int_timeout = 99999
@@ -57,7 +56,7 @@ def cactus_data(lst, exclude_value):
 
 # Main:
 if __name__ == '__main__':
-  text = "Takes a hex board"
+  text = "Takes a parsed grendel data"
   parser = argparse.ArgumentParser(description=text,formatter_class=argparse.RawTextHelpFormatter)
   parser.add_argument("--input", help="stats file path")
   args = parser.parse_args()
@@ -85,7 +84,7 @@ time_dict = dict()
 mem_dict = dict()
 
 
-df = pd.DataFrame(columns=["solver", "preprocessor", "format", "encoding", "instance", "mem", "time"], index=index_list,dtype=object)
+df = pd.DataFrame(columns=["solver", "preprocessor", "domain", "format", "encoding", "instance", "status", "mem", "time"], index=index_list,dtype=object)
 
 for i in range(len(stats_lines)):
 
@@ -110,31 +109,89 @@ for i in range(len(stats_lines)):
     solver = "CT"
   elif ("QU" in cur_line[0]):
     solver = "QU"
+  elif ("QB" in cur_line[0]):
+    solver = "QB"
 
   # format:
   if ("qcir" in cur_line[0]):
     format_type = "cir"
-  elif ("qdimacs" in cur_line[0]):
+  elif ("dimacs" in cur_line[0]):
     format_type = "qdim"
 
+  #format_type = "qdim"
+
+  '''
   # encoding:
-  if ("_ib" in cur_line[0]):
-    encoding = "ib"
+  if ("_bwnib" in cur_line[0]):
+    encoding = "bwnib"
   elif ("_nib" in cur_line[0]):
     encoding = "nib"
   elif ("_dnib" in cur_line[0]):
     encoding = "dnib"
+  '''
+
+  # encoding:
+  if ("_pgt_" in cur_line[0]):
+    encoding = "LT"
+  elif ("_EA_" in cur_line[0]):
+    encoding = "EA"
+  elif ("_ET_" in cur_line[0]):
+    encoding = "ET"
+  elif ("_EN_" in cur_line[0]):
+    encoding = "EN"
+  elif ("_LN_" in cur_line[0] or "_pg_" in cur_line[0] or "_LL_" in cur_line[0]):
+    encoding = "LN"
+  elif ("_SN_" in cur_line[0] or "_cp_" in cur_line[0]):
+    #print(cur_line[0])
+    encoding = "SN"
+  elif ("_COR_" in cur_line[0]):
+    encoding = "COR"
+  elif ("_LA_" in cur_line[0]):
+    encoding = "LA"
+  elif ("_SA_" in cur_line[0]):
+    encoding = "SA"
+
+  #print(encoding)
+
+
 
   # preprocessor:
-  if ("-B" in cur_line[0]):
+  if ("_B_" in cur_line[0]):
     preprocessor = "B"
+  elif("_H_" in cur_line[0]):
+    preprocessor = "H"
+  elif("_Q_" in cur_line[0]):
+    preprocessor = "Q"
   else:
     preprocessor = "N"
 
 
+  # domain:
+  '''
+  if ("_B_" in cur_line[0]):
+    domain = "B"
+  elif ("_BSP_" in cur_line[0]):
+    domain = "BSP"
+  elif ("_C4_" in cur_line[0]):
+    domain = "C4"
+  elif ("_EP_" in cur_line[0]):
+    domain = "EP"
+  elif ("_hex_" in cur_line[0]):
+    domain = "hex"
+  elif ("_httt_" in cur_line[0]):
+    domain = "httt"
+  else:
+    domain = "NA"
+  '''
+  domain = "NA"
+
+  status = cur_line[-3]
+  assert(status in ["UNSAT", "SAT", "NA"])
+  #status = "UNSAT"
+
   # instance name:
   instance = "N"
-  df.loc[i+1,:] = [solver, preprocessor, format_type, encoding, instance, mem, time]
+  df.loc[i+1,:] = [solver, preprocessor, domain, format_type, encoding, instance, status, mem, time]
 
   #print(solver, preprocessor, format_type, encoding, instance, mem, time)
 #print(df.to_string())
@@ -142,56 +199,208 @@ for i in range(len(stats_lines)):
 
 #print(len(df))
 print("=================================================================")
+print("SAT data")
+print("=================================================================")
+#===========================================================================================================================================================================
 #================================================================================================================================================
 # qcir solvers:
 #================================================================================================================================================
+# SAT instances:
 # checking circuit encodings:
-qcir_solvers = ["CT", "QU"]
-qcir_encodings = ["ib","nib","dnib"]
+#qcir_solvers = ["CT", "QU"]
+qcir_solvers = ["QB"]
+qcir_encodings = ["LN", "SN","LT"]
+preprocessors = ["B","N"]
 for qcir_encoding in qcir_encodings:
   for solver in qcir_solvers:
-    cur_subdf = df[(df['encoding'] == qcir_encoding) & (df['solver'] == solver) & (df['format'] == 'cir')]
-    #print(solver, qcir_encoding)
-    # 4 tuple as key: (encoding, solver, preprocessor and format):
-    time_dict[(qcir_encoding,solver,'N','cir')] = cur_subdf['time'].tolist()
-    # 4 tuple as key: (encoding, solver, preprocessor and format):
-    mem_dict[(qcir_encoding,solver,'N','cir')] = cur_subdf['mem'].tolist()
-    num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
-    print(qcir_encoding, solver,"  :  ", str(num_solved) +"/"+ str(len(time_dict[(qcir_encoding,solver,'N','cir')])), total_time, avg_time, mem_total, avg_mem)
+    for preprocessor in preprocessors:
+      #print(df)
+      cur_subdf = df[(df['encoding'] == qcir_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'cir') & (df['status'] == 'SAT')]
+      #print(solver, qcir_encoding)
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      time_dict[(qcir_encoding,solver, 'SAT', preprocessor,'cir')] = cur_subdf['time'].tolist()
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      mem_dict[(qcir_encoding,solver, 'SAT',preprocessor,'cir')] = cur_subdf['mem'].tolist()
+      num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
+      print(qcir_encoding, solver, preprocessor,"  :  ", str(num_solved), round(total_time, 2), avg_time, mem_total, avg_mem)
   print("------------------------------------------")
 
 print("=================================================================")
+
 #================================================================================================================================================
 # QDIMACS solvers:
 #================================================================================================================================================
 qdimacs_solvers = ["CQ","DQ"]
 # we take input preprocessor folder names, we also add empty string for no preprocessing:
-preprocessors = ["B"]
+preprocessors = ["B","H","Q","N"]
 #mpreprocessors =["M-N", "M-B", "M-H", "M-Q"]
 
-encodings = ["ib","nib","dnib"]
+#encodings = ["bwnib"]
+encodings = ["COR","EA","EN","ET","LN","SN","LT"]
+# checking qdimacs encodings:
+for expb_encoding in encodings:
+  for solver in qdimacs_solvers:
+    for preprocessor in preprocessors:
+      cur_subdf = df[(df['encoding'] == expb_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'qdim')& (df['status'] == 'SAT')]
+      time_dict[(expb_encoding,solver, "SAT" ,preprocessor,'qdim')] = cur_subdf['time'].tolist()
+      mem_dict[(expb_encoding,solver,"SAT", preprocessor,'qdim')] = cur_subdf['mem'].tolist()
+      num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
+      print(expb_encoding, solver, preprocessor, "  :  ", str(num_solved)   , round(total_time, 2), avg_time, mem_total, avg_mem)
+  print("------------------------------------------")
+print("=================================================================")
+print("UNSAT data")
+print("=================================================================")
+#=================================================================================================================================================
+# UNSAT instances:
+preprocessors = ["B","N"]
+# checking circuit encodings:
+for qcir_encoding in qcir_encodings:
+  for solver in qcir_solvers:
+    for preprocessor in preprocessors:
+      cur_subdf = df[(df['encoding'] == qcir_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'cir') & (df['status'] == 'UNSAT')]
+      #print(solver, qcir_encoding)
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      time_dict[(qcir_encoding,solver, 'UNSAT',preprocessor,'cir')] = cur_subdf['time'].tolist()
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      mem_dict[(qcir_encoding,solver, 'UNSAT',preprocessor,'cir')] = cur_subdf['mem'].tolist()
+      num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
+      print(qcir_encoding, solver, preprocessor,"  :  ", str(num_solved), round(total_time, 2), avg_time, mem_total, avg_mem)
+  print("------------------------------------------")
+
+print("=================================================================")
+
+#UNSAT
+preprocessors = ["B","H","Q","N"]
+# checking qdimacs encodings:
+for expb_encoding in encodings:
+  for solver in qdimacs_solvers:
+    for preprocessor in preprocessors:
+      cur_subdf = df[(df['encoding'] == expb_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'qdim')& (df['status'] == 'UNSAT')]
+      time_dict[(expb_encoding,solver, "UNSAT" ,preprocessor,'qdim')] = cur_subdf['time'].tolist()
+      mem_dict[(expb_encoding,solver,"UNSAT", preprocessor,'qdim')] = cur_subdf['mem'].tolist()
+      num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
+      print(expb_encoding, solver, preprocessor, "  :  ", str(num_solved)  , round(total_time, 2), avg_time, mem_total, avg_mem)
+  print("------------------------------------------")
+print("=================================================================")
+#===========================================================================================================================================================================
+#all data:
+print("=================================================================")
+print("ALL data")
+print("=================================================================")
+#===========================================================================================================================================================================
+# checking circuit encodings:
+#qcir_solvers = ["CT", "QU"]
+qcir_solvers = ["QB"]
+qcir_encodings = ["LN", "SN","LT"]
+preprocessors = ["B","N"]
+for qcir_encoding in qcir_encodings:
+  for solver in qcir_solvers:
+    for preprocessor in preprocessors:
+      cur_subdf = df[(df['encoding'] == qcir_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'cir')]
+      #print(solver, qcir_encoding)
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      time_dict[(qcir_encoding,solver, 'ALL',preprocessor,'cir')] = cur_subdf['time'].tolist()
+      # 4 tuple as key: (encoding, solver, preprocessor and format):
+      mem_dict[(qcir_encoding,solver, 'ALL',preprocessor,'cir')] = cur_subdf['mem'].tolist()
+      num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
+      print(qcir_encoding, solver, preprocessor,"  :  ", str(num_solved), round(total_time, 2), avg_time, mem_total, avg_mem)
+  print("------------------------------------------")
+
+print("=================================================================")
+
+#================================================================================================================================================
+# QDIMACS solvers:
+#================================================================================================================================================
+#qdimacs_solvers = ["CQ","DQ"]
+# we take input preprocessor folder names, we also add empty string for no preprocessing:
+#preprocessors = ["B","H","Q"]
+#mpreprocessors =["M-N", "M-B", "M-H", "M-Q"]
+
+#encodings = ["bwnib"]
+#encodings = ["COR","EA","EN","ET","LN","SN"]
+
+qdimacs_solvers = ["CQ","DQ"]
+# we take input preprocessor folder names, we also add empty string for no preprocessing:
+preprocessors = ["B","H","Q","N"]
+#mpreprocessors =["M-N", "M-B", "M-H", "M-Q"]
+
+#encodings = ["bwnib"]
+encodings = ["COR","EA","EN","ET","LN","SN","LT"]
 
 # checking qdimacs encodings:
 for expb_encoding in encodings:
   for solver in qdimacs_solvers:
     for preprocessor in preprocessors:
       cur_subdf = df[(df['encoding'] == expb_encoding) & (df['solver'] == solver) & (df['preprocessor'] == preprocessor) & (df['format'] == 'qdim')]
-      time_dict[(expb_encoding,solver,preprocessor,'qdim')] = cur_subdf['time'].tolist()
-      mem_dict[(expb_encoding,solver,preprocessor,'qdim')] = cur_subdf['mem'].tolist()
+      #print(expb_encoding,solver,preprocessor)
+      #print(cur_subdf['time'].tolist())
+      #print(cur_subdf['mem'].tolist())
+      time_dict[(expb_encoding,solver, "ALL" ,preprocessor,'qdim')] = cur_subdf['time'].tolist()
+      mem_dict[(expb_encoding,solver,"ALL", preprocessor,'qdim')] = cur_subdf['mem'].tolist()
       num_solved, total_time, avg_time, mem_total, avg_mem = accumulate(cur_subdf)
-      print(expb_encoding, solver, preprocessor, "  :  ", str(num_solved) +"/"+ str(len(time_dict[(expb_encoding,solver,preprocessor,'qdim')]))   , total_time, avg_time, mem_total, avg_mem)
+      print(expb_encoding, solver, preprocessor, "  :  ", str(num_solved)   , round(total_time, 2), avg_time, mem_total, avg_mem)
   print("------------------------------------------")
 print("=================================================================")
 
 
+base_markers = ['v','^','<','+','x','.','o']
+base_colors = ["b","g","r","c","m","k","y"]
+
+#base_markers = ['x','+','^','.','<','>']
+#base_colors = ["b","g","r","k","m","y"]
+
+labels = []
+
+# encoding combinations with both unsat and sat instances (total 6 variations):
+cur_encodings = list(encodings)
+cur_encodings = ["LN"]
+#cur_encodings = ["EA","EN","ET"]
+#cur_encodings = ["LN","SN"]
+best_combinations = []
+markers = []
+colors = []
+count = 0
+'''
+#------------------------------------------
+# qdimacs instances:
+format_t = "qdim"
+#cur_solvers = list(qdimacs_solvers)
+cur_solvers = ["DQ"]
+#cur_preprocessors = list(preprocessors)
+cur_preprocessors = ["B"]
+#------------------------------------------
+'''
+#------------------------------------------
+# qcir instances:
+format_t = "cir"
+cur_solvers = list(qcir_solvers)
+cur_preprocessors = ["N"]
+#------------------------------------------
 #'''
-best_combinations = [("dnib","CQ","B","qdim"),("dnib","DQ","B","qdim"),("dnib","CT","N","cir"), ("dnib","QU","N","cir"),("nib","CQ","B","qdim"),("nib","DQ","B","qdim"),("nib","QU","N","cir")]
-labels = ["dcq","ddq","dct","dqu","ncq","ndq","nqu"]
 
+for cur_encoding in cur_encodings:
+  for solr in cur_solvers:
+    for prep in cur_preprocessors:
+      best_combinations.append((cur_encoding,solr,"ALL",prep,format_t))
+      labels.append(cur_encoding + "_" + solr + "_" + prep)
+      markers.append(base_markers[count])
+      colors.append(base_colors[count])
+      count = count + 1
+#best_combinations = [("COR","CQ","ALL","B","qdim"),("COR","CQ","ALL","H","qdim"), ("COR","CQ","ALL","Q","qdim")]
 
+#print(best_combinations)
+
+#markers = ['x','+','^','.','<','>']
+#colors = ["b","g","r","k","m","y"]
 markers = ['v','^','<','+','x','.','o']
 colors = ["b","g","r","c","m","k","y"]
-#'''
+
+'''
+
+labels = ["COR_DQ_B","EA_CQ_B", "EN_CQ_Q", "ET_CQ_Q", "LN_CQ_B","SN_CQ_B","ET_CQ_B"]
+best_combinations = [('COR', 'CQ', 'ALL', 'B', 'qdim'), ('EA', 'CQ', 'ALL', 'B', 'qdim'), ('EN', 'CQ', 'ALL', 'B', 'qdim'),
+                     ('ET', 'CQ', 'ALL', 'B', 'qdim'), ('LN', 'CQ', 'ALL', 'B', 'qdim'), ('SN', 'CQ', 'ALL', 'B', 'qdim'),('LT', 'CQ', 'ALL', 'B', 'qdim')]
+'''
 
 #=======================================================================================
 # time plot:
@@ -200,6 +409,9 @@ colors = ["b","g","r","c","m","k","y"]
 for index in range(len(best_combinations)):
   b_comb = best_combinations[index]
   time_lst, count_lst =  cactus_data(time_dict[(b_comb)], int_timeout)
+  print(best_combinations[index])
+  print(time_lst)
+  print(count_lst)
   plt.scatter(count_lst, time_lst, label = labels[index],marker = markers[index], color=colors[index])
 
 plt.grid()
@@ -211,16 +423,33 @@ plt.show()
 #'''
 #=======================================================================================
 
+'''
+cor_base_CQ_Q = [0.87,57.45,719,0.01,0.01,10.2,15.5,0.3,4.31,4.13,104,0.01,0.11,367,4906,5.17,240,0.02,0.11,0.24,15.6,0.38,19,3247,26938,651,8964,2.29,44.4]
+cor_CQ_Q = [1,31,330,0.1,0.1,3,12,0.1,3,5,71,0.1,0.1,103,2229,6,58,0.1,0.1,0.1,12,1,19,795,3294,164,3350,2,24]
+
+plt.scatter(cor_base_CQ_Q, cor_CQ_Q)
+
+plt.grid()
+plt.xlabel("base")
+plt.ylabel("grendel run")
+plt.xscale('log')
+plt.yscale('log')
+#plt.legend()
+plt.show()
+'''
+
+
 #=======================================================================================
 # mem plot:
 #=======================================================================================
 '''
 for index in range(len(best_combinations)):
   b_comb = best_combinations[index]
+  print(best_combinations[index])
   #if ("EB" not in b_comb[0]):
   #  continue
   mem_lst, count_lst =  cactus_data(mem_dict[(b_comb)], mem_timeout)
-  print(mem_lst, count_lst,b_comb)
+  #print(mem_lst, count_lst,b_comb)
   #''
   if (mem_lst[0] == 1.4):
     mem_lst.pop(0)
