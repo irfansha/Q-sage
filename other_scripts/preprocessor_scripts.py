564f0be232493e738014acfcf7a8df5d7fe653f4
irfansha <irfansha536@gmail.com>
2023-01-23 10:21:46 +0100

    Small update in preprocessor scripts.

--- other_scripts/preprocessor_scripts.py
+++ other_scripts/preprocessor_scripts.py
@@ -28,12 +28,19 @@ if __name__ == '__main__':
   # Preprocessing
   #====================================================================================
   preprocessor_paths = ["/Bloqqer/bloqqer", "/HQSpre/hqspre", "/QRATPre+/qratpre+"]
+  #preprocessor_paths = ["/HQSpre/hqspre"]
   # we use these as paths for preprocessor output files:
   preprocessor_folders = ["/Bloqqer", "/HQSpre", "/QRATPre+"]
+  #preprocessor_folders = ["/HQSpre"]
   # paths for the qdimacs folders:
-  input_folders = ["/explicit_board_level", "/explicit_board_pg_format", "/explicit_board_white_disconnecting",
-                   "/qdimacs/lifted", "/qdimacs/witness", "/qdimacs/witness_black_legal",
-                   "/qdimacs/moved/lifted", "/qdimacs/moved/witness", "/qdimacs/moved/witness_black_legal"]
+  #input_folders = ["/explicit_board_level", "/explicit_board_pg_format", "/explicit_board_white_disconnecting",
+  #                "/qdimacs/lifted", "/qdimacs/witness", "/qdimacs/witness_black_legal",
+  #                "/qdimacs/moved/lifted", "/qdimacs/moved/witness", "/qdimacs/moved/witness_black_legal"]
+  input_folders = ["/EN","/ET","/LN","/SN","/LT"]
+  #input_folders = ["/LN","/SN", "/LT"]
+  #input_folders = ["/COR"]
+  #input_folders = ["/LT"]
+
   # We iterate through each preprocessor:
   for i in range(len(preprocessor_paths)):
     cur_path = args.preprocessor_dir + preprocessor_paths[i]
@@ -54,12 +61,6 @@ if __name__ == '__main__':
       os.mkdir(args.output_dir + preprocessor_folders[i] + "/qdimacs")
 
 
-    # moved folder is needed:
-    if not Path(args.output_dir + preprocessor_folders[i] + "/qdimacs/moved").is_dir():
-      print("Creating new " + preprocessor_folders[i] + "/qdimacs/moved directory in output folder.")
-      os.mkdir(args.output_dir + preprocessor_folders[i] + "/qdimacs/moved")
-
-
     # In each folder, we need specific folder for each encoding:
     for j in range(len(input_folders)):
       cur_encoding_folder = input_folders[j]
@@ -70,17 +71,18 @@ if __name__ == '__main__':
 
 
       files_list = glob.glob(args.input_dir + cur_encoding_folder + "/*")
+      print(args.input_dir + cur_encoding_folder + "/*")
       files_list.sort(key=natural_keys)
 
       #'''
       for file_path in files_list:
         only_file_name = file_path.split("/")[-1]
         if (preprocessor_folders[i] == "/Bloqqer"):
-          command = cur_path + " --timeout=100 " + file_path + " > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
+          command = "time " + cur_path + " --timeout=1000 " + file_path + " > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
         elif (preprocessor_folders[i] == "/HQSpre"):
-          command = cur_path + " --timeout 100 --pipe " + file_path + " > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
+          command = "time " + cur_path + " --timeout 1000 --pipe " + file_path + " > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
         elif (preprocessor_folders[i] == "/QRATPre+"):
-          command = cur_path + " --print-formula " + file_path + " 100 > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
+          command = "time " + cur_path + " --print-formula " + file_path + " 1000 > " + args.output_dir + preprocessor_folders[i] + cur_encoding_folder + "/" + only_file_name
         print(command)
         os.system(command)
     print("========================================================================")
